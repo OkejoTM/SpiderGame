0596b54d2129a1700ac3c69383a44063943814c8
Danila Kuzmin <denastytiko03@mail.ru>
2024-04-26 18:53:46 +0300

    fix actionEvents

--- src/Events/PlayerActionEvent.java
+++ src/Events/PlayerActionEvent.java
@@ -1,13 +1,31 @@
 package Events;
 
 import Setting.PlayerSpider;
+import Setting.WebCross;
 
 import java.util.EventObject;
 
 public class PlayerActionEvent extends EventObject {
 
     private PlayerSpider _player;
+    private WebCross _from;
+    private WebCross _to;
 
+    public void setFrom(WebCross from) {
+        _from = from;
+    }
+
+    public WebCross getFrom() {
+        return _from;
+    }
+
+    public void setTo(WebCross to) {
+        _to = to;
+    }
+
+    public WebCross getTo() {
+        return _to;
+    }
     public void setPlayer(PlayerSpider player){
         _player = player;
     }
